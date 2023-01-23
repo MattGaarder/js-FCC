a114a2dc124cea9ec1b8e0931238c226a093ad1e
MattGaarder <matteus.gaarder@gmail.com>
2023-01-23 19:59:07 +0000

    2 katas

--- codewars-1.js
+++ codewars-1.js
@@ -65,3 +65,85 @@ function litres(time) {
   var roundedAmountDrunk = Math.floor(amountDrunk)
   return roundedAmountDrunk;
 }
+
+
+// Arrow function: ES6
+// Anonymous functions are often used when a short-lived function is needed, 
+// and a name for the function is not required for readability or debugging. 
+// Anonymous functions are typically used as arguments to other functions or as callback functions.
+
+function(x) {
+  return x*2;
+};
+
+// equivalent to:
+
+x => x*2;
+
+// these values (arguments) are transformed or are sent to the function 
+
+// .filter();
+ const ages = [32, 33, 16, 40];
+ const result = ages.filter(checkAdult);
+
+ function checkAdult(age) {
+  return age >= 18;
+}
+
+// see how this is how anonymous functions are used.
+// Unlike the code above, we are putting a function as a parameter of the filter method
+// with x being the elements of the list 
+// this could be described using an arrow function:
+
+function filter_list(l) {
+  return l.filter(function(x) {
+        return typeof x === 'number';
+    });
+}
+
+filter_list([1,2,'a','b']) == [1,2]
+
+function filter_list(l) {
+  return l.filter(v => typeof v == "number")
+ }
+
+
+// Reversing an array 
+
+let myArray = [1, 2, 3, 4, 5];
+let reversedArray = [];
+for (let i = myArray.length - 1; i >= 0; i--) {
+    reversedArray.push(myArray[i]);
+}
+console.log(reversedArray);
+
+// i.myArray.length - 1 = 4; 
+// while i is greater than 0 
+// decrement i by one
+// push my array[4] into reversed array (the number 5)
+// push my array[3] into... (number 4) etc.
+
+function solution(str){
+  const splitStr = str.split("");
+  var reverseStrArray = [];
+  for(var i = splitStr.length - 1; i >= 0; i--){
+  reverseStrArray.push(splitStr[i]);  
+  }
+  return reverseStrArray.join("");
+}
+
+// .split("") The split() method splits a string into an array of substrings.
+// If (" ") is used as separator, the string is split between words.
+// string.split(separator, limit)
+// limit:	Optional.
+// An integer that limits the number of splits.
+// Items after the limit are excluded.
+// Alternate solution with arrow function:
+
+const solution = str => str.split('').reverse().join('');
+
+// or 
+
+function solution(str){
+  return str.split('').reverse().join('');  
+}
---
+++ notes-shortcuts.js
@@ -0,0 +1,4 @@
+// SHORTCUT: cmd + shift + H for find and replace all occurences of x
+// option + up/down to move line up or down
+// cmd + D for selecting repeating instances of x
+// shift + tab to unindent 
